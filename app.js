dc09d4f81a427e294023bd21878404f8042c452c
ArkejGit <robert.krawczyk92@gmail.com>
2018-12-15 20:28:57 +0100

    Move db configuration to separate file

--- app.js
+++ app.js
@@ -4,20 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('codegig', 'postgres', '346671alelol', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const db = require('./config/database');
 
 db.authenticate()
   .then(() => console.log('Database connected'))
---
+++ config/database.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('codegig', 'postgres', '346671alelol', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
